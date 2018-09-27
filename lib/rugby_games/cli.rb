952547d9c3c9061409b61c21efe64387f8817a6c
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-09-27 02:39:36 +0000

    update #list_games & #menu

--- lib/rugby_games/cli.rb
+++ lib/rugby_games/cli.rb
@@ -9,15 +9,15 @@ class RugbyGames::CLI
   def list_games
     puts "Today's rugby games"
   @games = RugbyGames::Matches.today
-  @games.each.with_index(1) do |game, i|
-    puts "#{i}. #{game.league} -- #{game.name} -- #{game.location}"
-    
+    @games.each.with_index(1) do |game, i|
+      puts "#{i}. #{game.league} -- #{game.name} -- #{game.location}"
+    end
   end
   
   def menu
     input = nil
     while input != "exit"
-    puts "Enter the list number to get more information about a match or type list to list games or type exit to end"
+      puts "Enter the list number to get more information about a match or type list to list games or type exit to end"
       input = gets.strip.downcase
       
       if input.to_i > 0 
