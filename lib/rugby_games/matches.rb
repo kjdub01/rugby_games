abaf51b977a90270ffc22effa1281b5ae9ccd222
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-09-27 02:40:49 +0000

    create Matches object

--- lib/rugby_games/matches.rb
+++ lib/rugby_games/matches.rb
@@ -1,10 +1,19 @@
 class RugbyGames::Matches
+  attr_accessor :name, :location, :league, :url
   
   def self.today
-  puts <<-DOC.gsub /^\s*/, ''
-    
-      1. Hawkes Bay vs Northland at McLean Park, Napier
-      2. New Zealand vs Argentina at Welington
-    DOC
+  match_1 = self.new 
+  match_1.name = "Hawkes Bay vs Northland"
+  match_1.location = "McLean Park, Napier" 
+  match_1.league = "Mitre 10 Cup"
+  match_1.url = "http://www.espn.com/rugby/match?gameId=292858&league=270563"
+  
+  match_2 = self.new 
+  match_2.name = "New Zealand vs Argentina"
+  match_2.location = "Wellington" 
+  match_2.league = "Friendly"
+  match_2.url = "http://www.espn.com/rugby/match?gameId=somethingleague=something"
+  
+  [match_1, match_2]
   end
 end
