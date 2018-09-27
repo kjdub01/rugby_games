093ceb50b36b4dff3520ab04eaf752636850942b
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-09-27 00:34:17 +0000

    create matches and #today

---
+++ lib/rugby_games/matches.rb
@@ -0,0 +1,10 @@
+class RugbyGames::Matches
+  
+  def self.today
+  puts <<-DOC.gsub /^\s*/, ''
+    
+      1. Hawkes Bay vs Northland at McLean Park, Napier
+      2. New Zealand vs Argentina at Welington
+    DOC
+  end
+end
