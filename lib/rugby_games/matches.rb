8caef4a16bc6b597266944da9d6a43e3f85032b5
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-09-28 19:00:10 +0000

    create ::scrape_matches & ::scrape_espn

--- lib/rugby_games/matches.rb
+++ lib/rugby_games/matches.rb
@@ -1,19 +1,18 @@
-class RugbyGames::Matches
+  class RugbyGames::Matches
   attr_accessor :name, :location, :league, :url
   
   def self.today
-  match_1 = self.new 
-  match_1.name = "Hawkes Bay vs Northland"
-  match_1.location = "McLean Park, Napier" 
-  match_1.league = "Mitre 10 Cup"
-  match_1.url = "http://www.espn.com/rugby/match?gameId=292858&league=270563"
+    self.scrape_matches
+  end
   
-  match_2 = self.new 
-  match_2.name = "New Zealand vs Argentina"
-  match_2.location = "Wellington" 
-  match_2.league = "Friendly"
-  match_2.url = "http://www.espn.com/rugby/match?gameId=somethingleague=something"
+  def self.scrape_matches
+    matches = []
+    
+    matches << self.scrape_espn
+  end
   
-  [match_1, match_2]
+  def self.scrape_espn
+    site = Nokogiri::HTML(open("http://www.espn.com/rugby/fixtures"))
+    binding.pry
   end
 end
