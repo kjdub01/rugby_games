6a904cefc6d033d0726c83427650d27ff7c5fed9
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-10-02 03:12:12 +0000

    update .scrape_espn

--- lib/rugby_games/matches.rb
+++ lib/rugby_games/matches.rb
@@ -12,9 +12,11 @@
   end
   
   def self.scrape_espn
-    doc = Nokogiri::HTML(open("http://www.espn.com/rugby/scoreboard?date=20180929"))
+    doc = Nokogiri::HTML(open("http://www.espn.com/rugby/scoreboard"))
     teams = doc.search('span.short-name').map(&:text).delete_if{|x| x !~ /\w/}
-    leagues = doc.search("h2.date-heading").map(&:text).delete_if{|x| x !~ /\w/}
+    leagues = doc.search('h2.date-heading').map(&:text).delete_if{|x| x !~ /\w/}
+    times = doc.search('span.game-time').map(&:text).delete_if{|x| x !~ /\w/}
+    fullgames = doc.search('a.competitors').map(&:text).delete_if{|x| x !~ /\w/}
     binding.pry
   end
 end
