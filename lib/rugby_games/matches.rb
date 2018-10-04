c0d1a65f663dcfe8fad1c0a67354b123fa865b32
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-10-04 05:10:16 +0000

    update scrape_espn

--- lib/rugby_games/matches.rb
+++ lib/rugby_games/matches.rb
@@ -12,17 +12,23 @@
   end
   
   def self.scrape_espn
-    doc = Nokogiri::HTML(open("http://www.espn.com/rugby/scoreboard"))
+    doc = Nokogiri::HTML(open("http://www.espn.com/rugby/scoreboard?date=20181003"))
     teams = doc.search('span.short-name').map(&:text).delete_if{|x| x !~ /\w/}
-    leagues = doc.search('h2.date-heading').map(&:text).delete_if{|x| x !~ /\w/}
     times = doc.search('span.game-time').map(&:text).delete_if{|x| x !~ /\w/}
-    fullgames = doc.search('a.competitors').map(&:text).delete_if{|x| x !~ /\w/}
+    leagues = doc.search('h2.date-heading').map(&:text).delete_if{|x| x !~ /\w/}
+    
+    a_teams = teams.values_at(*teams.each_index.select(&:even?))
+    b_teams = teams.values_at(*teams.each_index.select(&:odd?))
+    games = a_teams.zip(times, b_teams)
+    games.join(" ")
+    
+    binding.pry
   end
   
   def self.scrape_bbc
     doc = Nokogiri::HTML(open("https://www.bbc.com/sport/rugby-union/international-match/fixtures"))
     games = doc.search('a.fixture__block-link').map(&:text)
     dates = doc.search('h4.gel-minion').map(&:text)
-    binding.pry
+    
   end
 end
