00e6eb4056b6111b3dfa40eabe9b203603b157b4
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-10-07 02:28:41 +0000

    update scrape_espn

--- lib/rugby_games/matches.rb
+++ lib/rugby_games/matches.rb
@@ -1,18 +1,18 @@
-  class RugbyGames::Matches
+  class Games
   attr_accessor :name, :location, :league, :url
   
   def self.today
-    self.scrape_matches
+    self.scrape_games
   end
   
-  def self.scrape_matches
+  def self.scrape_games
     matches = []
     
     matches << self.scrape_espn
   end
   
   def self.scrape_espn
-    doc = Nokogiri::HTML(open("http://www.espn.com/rugby/scoreboard?date=20181003"))
+    doc = Nokogiri::HTML(open("http://www.espn.com/rugby/scoreboard?date=20181005"))
     teams = doc.search('span.short-name').map(&:text).delete_if{|x| x !~ /\w/}
     times = doc.search('span.game-time').map(&:text).delete_if{|x| x !~ /\w/}
     leagues = doc.search('h2.date-heading').map(&:text).delete_if{|x| x !~ /\w/}
@@ -21,14 +21,16 @@
     a_teams = teams.values_at(*teams.each_index.select(&:even?))
     b_teams = teams.values_at(*teams.each_index.select(&:odd?))
     games = a_teams.zip(times, b_teams)
-    games.collect { |game| game.join(" ") }
+    the_games = games.collect { |game| game.join(" ") }
     
-    game = Matches.new 
-    game.name = games.collect { |game| game.join(" ") }
-    game.url = 
+    
+    game = Games.new 
+    game.name = 
     
     binding.pry
   end
+  
+  
   
   
   def self.scrape_bbc
