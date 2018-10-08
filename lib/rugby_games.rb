6aeecb1479ea80ee032e2a0c80bea13a7be71837
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-10-08 17:56:43 +0000

    update

--- lib/rugby_games.rb
+++ lib/rugby_games.rb
@@ -2,6 +2,6 @@ require "nokogiri"
 require "pry"
 require "open-uri"
 require_relative "./rugby_games/version"
-require_relative "./rugby_games/matches"
+require_relative "./rugby_games/games"
 require_relative "./rugby_games/cli"
 
