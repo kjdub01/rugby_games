10b7a1721865e1440b1f2d4b1475799dffc68ddd
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-10-08 22:24:31 +0000

    update menu to provide scores

--- lib/rugby_games/cli.rb
+++ lib/rugby_games/cli.rb
@@ -23,7 +23,7 @@ class RugbyGames::CLI
       
       if input.to_i > 0 
         the_game = @games[input.to_i - 1]
-        puts "#{the_game.time} -- #{the_game.first_team} vs #{the_game.second_team}"
+        puts "#{the_game.first_team}  #{the_game.first_score}  #{the_game.time}  #{the_game.second_score}  #{the_game.second_team}"
         puts 
       elsif "list"
         list_games
