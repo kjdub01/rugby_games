be256956c85edfb941afdd99a9765056edfe5c8f
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-10-08 03:00:39 +0000

    update scrape_espn

--- lib/rugby_games/matches.rb
+++ lib/rugby_games/matches.rb
@@ -1,14 +1,14 @@
   class Games
-  attr_accessor :name, :location, :league, :url
+  attr_accessor :first_team, :second_team, :time, :league, :url
+  
+  @@all = []
   
   def self.today
-    self.scrape_games
+    @@all
   end
   
-  def self.scrape_games
-    matches = []
-    
-    matches << self.scrape_espn
+  def save 
+    @@all << self
   end
   
   def self.scrape_espn
@@ -20,12 +20,21 @@
     
     a_teams = teams.values_at(*teams.each_index.select(&:even?))
     b_teams = teams.values_at(*teams.each_index.select(&:odd?))
-    games = a_teams.zip(times, b_teams)
-    the_games = games.collect { |game| game.join(" ") }
+    #games = a_teams.zip(times, b_teams)
+    #the_games = games.collect { |game| game.join(" ") }
     
     
-    game = Games.new 
-    game.name = 
+    (0..a_teams.size).to_a.each do |i|
+      a_team_name = a_teams[i]
+      b_team_name = b_teams[i]
+      time = times[i]
+      
+      game = Games.new 
+      game.first_team = a_team_name
+      game.second_team = b_team_name
+      game.time = time
+      game.save
+    end
     
     binding.pry
   end
