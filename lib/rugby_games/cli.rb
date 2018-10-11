0f9948fa593d423add05d719c0ef8dd219b86818
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-10-11 03:28:54 +0000

    fix if statement return value in #menu

--- lib/rugby_games/cli.rb
+++ lib/rugby_games/cli.rb
@@ -10,13 +10,11 @@ class RugbyGames::CLI
     else
       list_games
       menu
-      goodbye
     end
   end 
   
   def list_games
     puts "Today's Rugby Games"
-    
     @games.each.with_index(1) do |game, i|
       puts "#{i}. #{game.time} -- #{game.first_team} vs #{game.second_team}"
     end
@@ -28,19 +26,20 @@ class RugbyGames::CLI
       puts "Enter the list number to get more information about a match or type list to list games or type exit to end"
       input = gets.strip.downcase
       
-      if input.to_i > 0 
+      if input.to_i > 0 && input.to_i < @games.size
         the_game = @games[input.to_i - 1]
         puts "#{the_game.first_team}  #{the_game.first_score}  #{the_game.time}  #{the_game.second_score}  #{the_game.second_team}"
-        puts 
-      elsif "list"
+      elsif input == "list"
         list_games
-      else
+      elsif input == "exit"
+        goodbye
+      else 
         puts "Invalid entry."
       end
     end
   end 
   
   def goodbye
-    puts "Come back for more games tomorrow"
+    puts "Come back for more games tomorrow!"
   end
 end
