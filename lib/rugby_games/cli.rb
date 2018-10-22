ffa82053a02310c5e80cedefae3ae8a98fd35418
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-10-22 03:00:33 +0000

    update scrape_espn and variable names

--- lib/rugby_games/cli.rb
+++ lib/rugby_games/cli.rb
@@ -16,7 +16,7 @@ class RugbyGames::CLI
   def list_games
     puts "Today's Rugby Games"
     @games.each.with_index(1) do |game, i|
-      puts "#{i}. #{game.time} -- #{game.first_team} vs #{game.second_team}"
+      puts "#{i}. #{game.time} -- #{game.home_team} vs #{game.away_team}"
     end
   end
   
@@ -26,9 +26,9 @@ class RugbyGames::CLI
       puts "Enter the list number to get more information about a match or type list to list games or type exit to end"
       input = gets.strip.downcase
       
-      if input.to_i > 0 && input.to_i < @games.size
+      if input.to_i > 0 && input.to_i <= @games.size
         the_game = @games[input.to_i - 1]
-        puts "#{the_game.first_team}  #{the_game.first_score}  #{the_game.time}  #{the_game.second_score}  #{the_game.second_team}"
+        puts "#{the_game.home_team}  #{the_game.home_score}  #{the_game.time}  #{the_game.away_score}  #{the_game.away_team}"
       elsif input == "list"
         list_games
       elsif input == "exit"
--- lib/rugby_games/games.rb
+++ lib/rugby_games/games.rb
@@ -1,10 +1,10 @@
 class Games
-  attr_accessor :first_team, :second_team, :time, :first_score, :second_score
+  attr_accessor :home_team, :away_team, :time, :home_score, :away_score, :url
   
   @@all = []
   
   def self.today
-    @@all.reverse.drop(1).reverse
+    @@all
   end
   
   def save 
@@ -12,37 +12,24 @@ class Games
   end
   
   def self.scrape_espn
-    doc = Nokogiri::HTML(open("http://www.espn.com/rugby/scoreboard?"))
-    teams = doc.search('span.short-name').map(&:text).delete_if{|x| x !~ /\w/}
-    times = doc.search('span.game-time').map(&:text)
-    scores = doc.search('div.score-container').map(&:text)
-    
-    
-    games = doc.search("div#events")
-    games.each do |game|
-      league_name = game.css("div a h2.date-heading.js-show").text 
-      binding.pry
-    end
-  
-    a_teams = teams.values_at(*teams.each_index.select(&:even?))
-    b_teams = teams.values_at(*teams.each_index.select(&:odd?))
-    a_teams_scores = scores.values_at(*teams.each_index.select(&:even?))
-    b_teams_scores = scores.values_at(*teams.each_index.select(&:odd?))
+    doc = Nokogiri::HTML(open("http://www.espn.com/rugby/scoreboard"))
    
-    (0..a_teams.size).to_a.each do |i|
-      a_team_name = a_teams[i]
-      b_team_name = b_teams[i]
-      time = times[i]
-      a_team_score = a_teams_scores[i]
-      b_teams_score = b_teams_scores[i]
-      
-      game = Games.new 
-      game.first_team = a_team_name
-      game.second_team = b_team_name
-      game.time = time
-      game.first_score = a_team_score
-      game.second_score = b_teams_score
+    doc.search("a.competitors").each do |x|
+      game = Games.new
+      game.url = x.attr("href")
+      game.time = x.css("span.game-time").text
+      game.home_team = x.css("div.team.team-a.possession span.short-name").text
+      game.home_score = x.css("div.team.team-a.possession div.score.icon-font-after").text
+      game.away_team = x.css("div.team.team-b.possession span.short-name").text
+      game.away_score = x.css("div.team.team-b.possession div.score.icon-font-before").text
       game.save
     end
+  
+    doc.search("div#events").each do |league|
+      game.league_name = league.css("div a h2.date-heading.js-show").text 
+      game.save
+    end
+  
+   binding.pry
   end
 end
