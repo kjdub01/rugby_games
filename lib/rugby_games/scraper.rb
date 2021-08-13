6a97b2b18e1610817dd0c433abafa30fa0c5d41a
kjdub01 <kjewettwilliams@gmail.com>
2021-08-12 21:12:43 -0700

    scrape date from carousel instead if each game & add game url variable

--- lib/rugby_games/scraper.rb
+++ lib/rugby_games/scraper.rb
@@ -2,14 +2,17 @@ class RugbyGames::Scraper
   BASE_URL = "http://www.espn.com"
   def self.scrape_espn
     @doc = Nokogiri::HTML(URI.open("http://www.espn.com/rugby/scoreboard?"))
+
+    @doc.css("div.carousel-day").each do |date|
+      games_date = RugbyGames::Dates.new
+      games_date.text = date.css("span#sbpDate.date").text
+      games_date.save
+    end
    
     @doc.css("a.competitors").each do |contest|
-      date = RugbyGames::Dates.new
-      date.text = contest.css("span.game-date").text
-      date.save
 
       game = RugbyGames::Games.new
-      #game.url = BASE_URL + contest.attr("href")
+      game.url = BASE_URL + contest.attr("href")
       game.time = contest.css("span.game-time").text
       game.home_team = contest.css("div.team.team-a.possession span.short-name").text
       game.home_score = contest.css("div.team.team-a.possession div.score.icon-font-after").text
